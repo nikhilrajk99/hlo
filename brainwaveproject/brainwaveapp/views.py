1afe8ec539cd0552a3e2d536d325a0158da14131
nikhilrajk99 <nikhilkannan2017@gmail.com>
2023-04-22 14:46:02 +0530

    first commit

---
+++ brainwaveproject/brainwaveapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
+def portfolio(request):
+    return render(request,"portfolio.html")
+def service(request):
+    return render(request,"service.html")
